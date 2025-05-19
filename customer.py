2acdec4c1276bc336a861dd2f27f34a2af81efb2
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-19 13:20:31 +0300

    added the create order method

--- customer.py
+++ customer.py
@@ -5,18 +5,40 @@ class Customer:
         self.name = name
         self._orders = []
         Customer.all_customers.append(self)
-@property
-def name(self):
+
+    @property
+    def name(self):
         return self._name
 
-@name.setter
-def name(self, value):
+    @name.setter
+    def name(self, value):
         if not isinstance(value, str):
             raise ValueError("Name must be a string")
         if not 1 <= len(value) <= 15:
             raise ValueError("Name must be between 1 and 15 characters")
         self._name = value
-def orders(self):
+
+    def orders(self):
         return self._orders
-def coffees(self):
+
+    def coffees(self):
         return list({order.coffee for order in self._orders})
+
+    def create_order(self, coffee, price):
+        new_order = Order(self, coffee, price)
+        self._orders.append(new_order)
+        return new_order
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        if not coffee.orders():
+            return None
+            
+        customers_spending = {}
+        for order in coffee.orders():
+            if order.customer in customers_spending:
+                customers_spending[order.customer] += order.price
+            else:
+                customers_spending[order.customer] = order.price
+                
+        return max(customers_spending.items(), key=lambda x: x[1])[0]
